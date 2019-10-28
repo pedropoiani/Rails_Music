a3d94d8d53171ab5ec8c75a66974356effc5dca1
Marcel Rizzo <42222312+Mbrizzo@users.noreply.github.com>
2019-10-28 15:52:49 -0300

    AddNewTrackController
    Criado o controller para novas músicas

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,5 +6,36 @@ class TracksController < ApplicationController
                     @track = Track.all
                 
                 end
+ 
+                def show
+                    @track = Track.find(params[:id])
+                end
 
-            end
+                def new 
+                    @track = Track.new
+                end
+
+                def create
+                    @track = Track.new(track_params)
+                    if @track.save
+                      redirect_to @track
+                    else
+                      render :new
+                    end
+                end
+
+                def edit
+
+                    @track = Track.find(track_params[:id])
+                 
+                end
+
+                private
+
+                def track_params
+                    params.require(:track).permit(:name, :album, :media_type, :genre,
+                                                  :milliseconds, :bytes, :unit_price)
+                end
+
+
+end
